16d69b8a7c34ab734972d10873ab6b21bd5f1eef
Diego Medina Mauricio <hacker290490@gmail.com>
2023-02-16 12:00:04 +0100

    Añadimos el metodo getCounter

--- Spring3/Tema3-Patterns/Nivel1/tigger.php
+++ Spring3/Tema3-Patterns/Nivel1/tigger.php
@@ -4,6 +4,7 @@ class Tigger
 {
     //1. Necesitamos una variable estatica del patron
     private static $instance = null;
+    private $contador = 0;
 
     //2.Añadimos el modificador de acceso PRIVATE para evitar el acceso del constructor fuera de esta clase. Es decir no podran hacer un NEW fuera de la clase.
     private function __construct()
@@ -12,24 +13,31 @@ class Tigger
     }
 
     //3. Esta funcion comprueba que la variable instancia sea nula. Y solo permita instanciar la clase mediante esta funcion. 
-
     //Self hace referencia a la clase actual y se usando cuando instancia dicha clase, es decir se usan métodos estáticos.
     //This hace referencia al objeto actual, es decir, cuando una clase si tiene instancia. No se puede hacer referencia a métodos estáticos usando this pero si a métodos públicos, privados y protegidos.
-    public static function getInstance(){
-        if( null === self::$instance){
+    public static function getInstance()
+    {
+        if (null === self::$instance) {
             self::$instance = new Tigger();
         }
 
         return self::$instance;
     }
 
+    // Añadimos el modificador de acceso a la función CLONE que nos permite clonar un objeto. Ya que solo queremos instanciar a traves de la funcion getInstance. 
     private function __clone()
     {
-        
     }
 
     public function roar()
     {
         echo "Grrr!" . PHP_EOL;
+
+        //$this->getCounter();
+    }
+
+    public function getCounter()
+    {
+        return ++$this->contador;
     }
 }
