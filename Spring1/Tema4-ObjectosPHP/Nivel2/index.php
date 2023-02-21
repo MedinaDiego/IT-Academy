05eb6c84df65a58aeb38abe0da8e43605a7f15bc
Diego Medina Mauricio <hacker290490@gmail.com>
2023-02-21 12:14:51 +0100

    actualización PokerDice

--- Spring1/Tema4-ObjectosPHP/Nivel2/index.php
+++ Spring1/Tema4-ObjectosPHP/Nivel2/index.php
@@ -1,4 +1,20 @@
 <?php 
 require_once 'pokerdice.php';
 
-$jugada = new pokerdice();
+$mano = array(); 
+$numeroDados = 0;
+$jugada = 0;
+
+getTotalThrows($numeroDados,$mano, $jugada);
+
+function getTotalThrows($numeroDados,$mano, $jugada){
+    while($numeroDados < 5){
+        $tiro = new pokerdice();
+        $tiro->th_row();
+        $resultado = $tiro->shapename();
+        array_push($mano, $resultado);
+        $numeroDados += 1;
+    }
+
+    print_r($mano);
+}
--- Spring1/Tema4-ObjectosPHP/Nivel2/pokerdice.php
+++ Spring1/Tema4-ObjectosPHP/Nivel2/pokerdice.php
@@ -1,17 +1,44 @@
 <?php
 
-Class pokerdice extends dado{
-    public $cara;
-    public function __construct(){
+class pokerdice
+{
+    private $num_aleatorio = 0;
+    private $cara;
 
+    /*public function __construct()
+    {
+    }*/
+
+    public function th_row()
+    {
+        return $this->num_aleatorio = mt_rand(1, 6);
     }
 
-    public function th_row(){
-       return $cara = mt_rand(1,6);
-    }
-
-    public function shapename(){
-
+    public function shapename()
+    {
+        
+        switch ($this->num_aleatorio) {
+            case 6:
+                return $this->cara = "AS";
+                break;
+            case 5:
+                return $this->cara = "K";
+                break;
+            case 4:
+                return $this->cara = "Q";
+                break;
+            case 3:
+                return $this->cara = "J";
+                break;
+            case 2:
+                return $this->cara = 8;
+                break;
+            case 1:
+                return $this->cara = 7;
+                break;
+            default:
+                echo "error";
+        }
     }
 
 }
