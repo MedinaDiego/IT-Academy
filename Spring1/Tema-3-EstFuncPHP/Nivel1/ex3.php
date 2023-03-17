bbf640732235a555a18bd922dd9a52f1b266859d
Diego Medina <medinadiego29@icloud.com>
2023-03-17 12:59:04 +0100

    Añadimos una const para definir un numero predeterminado.

--- Spring1/Tema-3-EstFuncPHP/Nivel1/ex3.php
+++ Spring1/Tema-3-EstFuncPHP/Nivel1/ex3.php
@@ -1,8 +1,12 @@
 <?php
-$n = 20;
+define("AMAGATALL", 10);
+$n = 0;
 comptar($n);
 function comptar($n)
 {
+    if (is_null($n) || $n == 0) {
+        $n = AMAGATALL;
+    }
     for ($i = 1; $i < $n; $i++) {
         $i += 1;
         echo $i . "\n";
